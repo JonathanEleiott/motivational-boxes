c25676ee8fe358966d937b0a7313dea6a8be93f0
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2022-12-19 10:25:26 -0500

    Added name input functionality

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // DOM SELECTORS
-const div = document.querySelector('div');
+const motivationalQuoteDiv = document.querySelector('div');
+const enterNameButton = document.querySelector('#set-name');
+const lastNameEnteredH3 = document.querySelector('h3');
+const nameInput = document.getElementById('name');
 
 // EVENT LISTENERS
-div.addEventListener('click', () => {
+motivationalQuoteDiv.addEventListener('click', () => {
   console.log('You are the best!');
-})
+});
+
+enterNameButton.addEventListener('click', () => {
+  const message = 'Last name entered is: ';
+  lastNameEnteredH3.innerText = `${message} ${nameInput.value}`;
+  nameInput.value = '';
+});
